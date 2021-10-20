1d928acf8c54e9c18a497b4601352cf2425982d4
yashpatil17 <yash@platform9.com>
2021-10-20 17:35:20 +0530

    Use curl instead of wget

--- resources/vbmc/download-certificates.sh
+++ resources/vbmc/download-certificates.sh
@@ -1,5 +1,5 @@
 mkdir -p /etc/pf9/certs/libvirt/
 cd /etc/pf9/certs/libvirt/
-wget https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/ca/cert.pem -O cacert.pem
-wget https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/cert.pem -O clientcert.pem
-wget https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/key.pem -O clientkey.pem
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/ca/cert.pem -o cacert.pem
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/cert.pem -o clientcert.pem
+curl https://artifactory.platform9.horse/artifactory/pf9-bins/ironic/df-us-mpt1-kvm/certs/v2/hostagent/key.pem -o clientkey.pem
